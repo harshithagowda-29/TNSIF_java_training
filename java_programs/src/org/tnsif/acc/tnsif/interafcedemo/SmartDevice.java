5bb92fb6d56252e537e729247b82a006e0e2b76f
user <harshithagouda29@gmail.com>
2026-08-17 11:31:35 +0530

    frist commit

---
+++ java_programs/src/org/tnsif/acc/tnsif/interafcedemo/SmartDevice.java
@@ -0,0 +1,8 @@
+package org.tnsif.acc.tnsif.interafcedemo;
+
+public interface SmartDevice { 
+	public abstract void turnOn();
+	public abstract void turnOff();
+	public abstract void getStatus();
+
+}
